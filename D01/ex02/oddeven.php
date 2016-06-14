6cd498e75990decfc4250ec6e77814683902a3e9
tfolly <tfolly@student.42.fr>
2016-06-14 14:22:17 +0200

    oddeven

---
+++ D01/ex02/oddeven.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php
+<?php
+
+function read_stdin()
+{
+        $fr=fopen("php://stdin","r");
+        $input = fgets($fr);
+        $input = rtrim($input);
+        fclose ($fr);
+        return $input;
+}
+
+
+
+while (1)
+{
+	echo "Entrez un nombre: ";
+	$stdin = read_stdin();
+	if(is_numeric($stdin))
+	{
+		if ($stdin % 2 == 0)
+			print("Le chiffre $stdin est Pair\n");
+		else
+			print("Le chiffre $stdin est Impair\n");
+	}
+	else
+	{
+		print("'$stdin' n'est pas un chiffre\n");
+	}
+}
+
+?>
