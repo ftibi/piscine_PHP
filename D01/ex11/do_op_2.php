35e5b39a5071a58ea7d50052d5c1548306259218
tfolly <tfolly@student.42.fr>
2016-06-14 18:57:11 +0200

    do_op2

--- D01/ex11/do_op_2.php
+++ D01/ex11/do_op_2.php
@@ -1,26 +1,29 @@
 #!/usr/bin/php
 <?php
 
-//reste a penser si plsr + ?
-
-function only_one_op($str)
+function sum($val1, $val2)
 {
-	$op = 0;
-	if (strpos($str, '+'))
-		$op++;
-	if (strpos($str, '-'))
-		$op++;
-	if (strpos($str, '*'))
-		$op++;
-	if (strpos($str, '/'))
-		$op++;
-	if (strpos($str, '%'))
-		$op++;
-	if ($op != 1)
-	{
-		echo "Syntax Error\n";
-		exit (0);
-	}
+	return ($val1 + $val2);
+}
+
+function sub($val1, $val2)
+{
+	return ($val1 - $val2);
+}
+
+function mult($val1, $val2)
+{
+	return ($val1 * $val2);
+}
+
+function div($val1, $val2)
+{
+	return ($val1 / $val2);
+}
+
+function mod($val1, $val2)
+{
+	return ($val1 % $val2);
 }
 
 if ($argc != 2)
@@ -29,6 +32,52 @@ if ($argc != 2)
 	return (0);
 }
 $str = (string)$argv[1];
-only_one_op($str);
+$str = str_replace(' ', '', $str);
+$str = str_replace('	', '', $str);
+$i = 0;
+while ($i < count($str))
+{
+	while (is_numeric($str[$i]))
+		$i++;
+	$val1 = intval(substr($str, 0, $i));
+	switch ($str[$i])
+	{
+		case '+':
+			$func = 'sum';
+			break;
+		case '-':
+			$func = 'sub';
+			break;
+		case '*':
+			$func = 'mult';
+			break;
+		case '/':
+			$func = 'div';
+			break;
+		case '%':
+			$func = 'mod';
+			break;
+		default:
+			echo "Syntax Error\n";
+			exit (0);
+	}
+	if (!ctype_digit(substr($str, $i + 1)))
+	{
+		echo "Syntax Error\n";
+		return (0);
+	}
+	$val2 = intval(substr($str, $i + 1));
+	if (is_numeric($val2))
+	{
+		$res = $func($val1, $val2);
+		echo "$res\n";
+		return (0);
+	}
+	else
+	{
+		echo "Syntax Error\n";
+		return (0);
+	}
+}
 
 ?>
