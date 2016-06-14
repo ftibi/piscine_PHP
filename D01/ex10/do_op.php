626dbb6943087c213fb32ebb6d8613bde68f1837
tfolly <tfolly@student.42.fr>
2016-06-14 17:17:31 +0200

    test do_op

---
+++ D01/ex10/do_op.php
@@ -0,0 +1,23 @@
+#!/usr/bin/php
+<?php
+
+if ($argc != 4)
+{
+	echo "Incorrect Parameters\n";
+	return 0;
+}
+$val1 = $argv[1];
+$val2 = $argv[3];
+
+if (trim($argv[2]) == '+')
+	 print($val1 + $val2."\n");
+if (trim($argv[2]) == '-')
+	 print($val1 - $val2."\n");
+if (trim($argv[2]) == '/')
+	 print($val1 / $val2."\n");
+if (trim($argv[2]) == '*')
+	 print($val1 + $val2."\n");
+if (trim($argv[2]) == '%')
+	 print($val1 % $val2."\n");
+
+?>
