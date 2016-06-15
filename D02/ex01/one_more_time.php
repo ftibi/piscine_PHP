b57f8dfb242bf2296ff38f18f583215ea68704c1
tfolly <tfolly@student.42.fr>
2016-06-15 16:31:25 +0200

    reste a verifier que le format est correct

--- D02/ex01/one_more_time.php
+++ D02/ex01/one_more_time.php
@@ -1,8 +1,23 @@
 #!/usr/bin/php
 <?php
 
-$date = explode(' ', $argv[1]);
-print_r("$date\n");
+if ($argc != 2)
+	return (0);
 
+//Penser a verifier que le format est valide
+
+//settings
+date_default_timezone_set('Europe/Paris');
+
+$month = array("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet",
+			"Aout", "Septembre", "Octobre", "Novembre", "Decembre");
+array_unshift($month, "toto");
+unset($month[0]);
+$month = array_flip($month);
+$date = explode(' ', $argv[1]);
+$time = explode(':', $date[4]);
+$res = mktime($time[0], $time[1], $time[2], $month[$date[2]], $date[1], $date[3]);
+
+echo "$res\n";
 
 ?>
