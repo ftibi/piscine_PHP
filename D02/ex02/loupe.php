ae721f3602b115b21e522f0753b89e525dccadf2
tfolly <tfolly@student.42.fr>
2016-06-16 20:16:28 +0200

    loupe OK

--- D02/ex02/loupe.php
+++ D02/ex02/loupe.php
@@ -1,6 +1,23 @@
 #!/usr/bin/php
 <?php
 
-// regex non autorisees omfg
+$fd = fopen($argv[1], 'r');
+$str = "";
+while ($new = fgets($fd))
+	$str = $str.$new;
+fclose($fd);
+
+$pattern = '/title="(.*?)"/';
+$pattern2 = '/<a.+>(.+?)</';
+function func($match)
+{
+	$match[0] = preg_replace("/".$match[1]."/", strtoupper($match[1]), $match[0]);
+	return ($match[0]);
+}
+
+$str = preg_replace_callback($pattern, "func", $str);
+$str = preg_replace_callback($pattern2, "func", $str);
+
+echo "$str";
 
 ?>
