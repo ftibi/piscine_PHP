3da455d321bbc5f1f955a30c87963adfe42e7bb6
tfolly <tfolly@student.42.fr>
2016-06-17 19:02:39 +0200

    looks ok

--- D04/ex01/create.php
+++ D04/ex01/create.php
@@ -1,14 +1,41 @@
 <?php
 
 session_start();
-if ($_GET['login'])
-	$_SESSION['login'] = $_GET['login'];
-if ($_GET['passwd'])
-	$_SESSION['passwd'] = $_GET['passwd'];
+if ($_POST['login'])
+	$_SESSION['login'] = $_POST['login'];
+if ($_POST['passwd'])
+	$_SESSION['passwd'] = $_POST['passwd'];
 
 if ($_POST['login'] && $_POST['passwd'])
-	return ("OK\n");
+{
+
+	if (!file_exists("./private"))
+		mkdir("./private", 0777, true);
+
+	$data_file = "";
+	$finename = "./private/passwd";
+	if (file_exists($finename))
+		$data_file = file_get_contents($finename, true);
+	else
+		$data_file = "";
+	$data_file = unserialize($data_file);
+
+
+	if ($data_file[$_SESSION['login']])
+	{
+			echo ("ERROR\n");
+			exit();
+	}
+	else
+	{
+		echo ("OK\n");
+		$data_file[$_SESSION['login']] = hash("whirlpool", $_SESSION['passwd']);
+		$data_file = serialize($data_file);
+	}
+	file_put_contents($finename, $data_file);
+
+}
 else
-	return ("ERROR\n");
+	echo ("ERROR\n");
 
 ?>
