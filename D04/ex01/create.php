8713ca07a06dac6e70e45f4bce7055568cafad8b
tfolly <tfolly@student.42.fr>
2016-06-17 22:08:00 +0200

    fix bug, mauvais serialize data

--- D04/ex01/create.php
+++ D04/ex01/create.php
@@ -1,7 +1,5 @@
 <?php
 
-session_start();
-
 if ($_POST['login'] && $_POST['passwd'])
 {
 
@@ -26,8 +24,8 @@ if ($_POST['login'] && $_POST['passwd'])
 	{
 		echo ("OK\n");
 		$data_file[$_POST['login']] = array("login" => $_POST['login'], "passwd" => hash("whirlpool", $_POST['passwd']));
-		$data_file = serialize($data_file);
 	}
+	$data_file = serialize($data_file);
 	file_put_contents($finename, $data_file);
 
 }
--- D04/ex04/create.php
+++ D04/ex04/create.php
@@ -1,14 +1,11 @@
 <?php
 
-session_start();
-
-if ($_POST['login'] && $_POST['passwd'])
+if (isset($_POST['login']) && isset($_POST['passwd']))
 {
 
 	if (!file_exists("../private"))
 		mkdir("../private", 0777, true);
 
-	$data_file = "";
 	$finename = "../private/passwd";
 	if (file_exists($finename))
 		$data_file = file_get_contents($finename, true);
@@ -16,19 +13,18 @@ if ($_POST['login'] && $_POST['passwd'])
 		$data_file = "";
 	$data_file = unserialize($data_file);
 
-
 	if ($data_file[$_POST['login']])
 	{
 		header("Location : index.html");
-		echo ("ERROR\n");
+		echo ("ERROR0\n");
 	}
 	else
 	{
 		header("Location : index.html");
 		echo ("OK\n");
 		$data_file[$_POST['login']] = array("login" => $_POST['login'], "passwd" => hash("whirlpool", $_POST['passwd']));
-		$data_file = serialize($data_file);
 	}
+	$data_file = serialize($data_file);
 	file_put_contents($finename, $data_file);
 	exit();
 
@@ -36,7 +32,7 @@ if ($_POST['login'] && $_POST['passwd'])
 else
 {
 	header("Location : index.html");
-	echo ("ERROR\n");
+	echo ("ERROR1\n");
 	exit();
 }
 ?>
