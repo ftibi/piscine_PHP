48f73dfc7bfa0f0de583325f5d86508cd76b1146
tfolly <tfolly@student.42.fr>
2016-06-17 16:46:00 +0200

    on avance doucement

--- D04/ex00/index.php
+++ D04/ex00/index.php
@@ -1,20 +1,21 @@
 <?php
 
 session_start();
+
+
+$_SESSION['login'] = $_GET['login'];
+$_SESSION['passwd'] = $_GET['passwd'];
 $login = $_SESSION['login'];
 $passwd = $_SESSION['passwd'];
-echo <<<'EOD'
+
+// echo $_GET['login']."\n";
+// echo $_GET['passwd']."\n";
+
+?>
+
 <form action='index.php' method='get'>
-	<p> Nom d'utilisateur : <input type='text' name='login' value="<?php echo'$login'; ?>"/></p>
-	<p> Mot de passe : <input type='text' name='passwd' /></p>
+	<p> Nom d'utilisateur : <input type='text' name='login' value="<?php if ($login) echo $login;?>"/></p>
+	<p> Mot de passe : <input type='text' name='passwd' value="<?php if ($passwd) echo $passwd;?>"/></p>
 	<input type='submit' name='submit' value='OK' /></p>
 
 </form>
-EOD;
-
-$_SESSION['login'] = $_GET['login'];
-
-echo $_GET['login']."\n";
-echo $_GET['passwd']."\n";
-
-?>
