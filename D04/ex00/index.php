76ce294f8526c75bb4a37b76016acbda2b48bac7
tfolly <tfolly@student.42.fr>
2016-06-17 17:00:15 +0200

    jai un pblm, apparement nvlle session chaque fois

--- D04/ex00/index.php
+++ D04/ex00/index.php
@@ -2,9 +2,10 @@
 
 session_start();
 
-
-$_SESSION['login'] = $_GET['login'];
-$_SESSION['passwd'] = $_GET['passwd'];
+if ($_GET['login'])
+	$_SESSION['login'] = $_GET['login'];
+if ($_GET['passwd'])
+	$_SESSION['passwd'] = $_GET['passwd'];
 $login = $_SESSION['login'];
 $passwd = $_SESSION['passwd'];
 
@@ -14,8 +15,15 @@ $passwd = $_SESSION['passwd'];
 ?>
 
 <form action='index.php' method='get'>
-	<p> Nom d'utilisateur : <input type='text' name='login' value="<?php if ($login) echo $login;?>"/></p>
-	<p> Mot de passe : <input type='text' name='passwd' value="<?php if ($passwd) echo $passwd;?>"/></p>
+	<p> Identifiant: <input type='text' name='login' value="<?php if ($login) echo $login;?>"/></p>
+	<p> Mot de passe: <input type='text' name='passwd' value="<?php if ($passwd) echo $passwd;?>"/></p>
 	<input type='submit' name='submit' value='OK' /></p>
 
 </form>
+<!--
+<?php
+
+$_SESSION['login'] = $_GET['login'];
+$_SESSION['passwd'] = $_GET['passwd'];
+
+?> -->
