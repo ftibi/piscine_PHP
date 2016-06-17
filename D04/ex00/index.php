c3e15c1959b554e2938db6dd96ab830b907f76e3
tfolly <tfolly@student.42.fr>
2016-06-17 17:07:11 +0200

    tjr le pblm de session

--- D04/ex00/index.php
+++ D04/ex00/index.php
@@ -6,24 +6,15 @@ if ($_GET['login'])
 	$_SESSION['login'] = $_GET['login'];
 if ($_GET['passwd'])
 	$_SESSION['passwd'] = $_GET['passwd'];
-$login = $_SESSION['login'];
-$passwd = $_SESSION['passwd'];
 
-// echo $_GET['login']."\n";
-// echo $_GET['passwd']."\n";
+ echo $_SESSION['login']."\n";
+ echo $_SESSION['passwd']."\n";
 
 ?>
 
 <form action='index.php' method='get'>
-	<p> Identifiant: <input type='text' name='login' value="<?php if ($login) echo $login;?>"/></p>
-	<p> Mot de passe: <input type='text' name='passwd' value="<?php if ($passwd) echo $passwd;?>"/></p>
+	<p> Identifiant: <input type='text' name='login' value="<?php if ($_SESSION['login']) echo $_SESSION['login'];?>"/></p>
+	<p> Mot de passe: <input type='text' name='passwd' value="<?php if ($_SESSION['passwd']) echo $_SESSION['passwd'];?>"/></p>
 	<input type='submit' name='submit' value='OK' /></p>
 
 </form>
-<!--
-<?php
-
-$_SESSION['login'] = $_GET['login'];
-$_SESSION['passwd'] = $_GET['passwd'];
-
-?> -->
