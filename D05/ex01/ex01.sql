4dd202b1fef167231e42dbcecd8c95da066a48f9
tfolly <tfolly@student.42.fr>
2016-06-20 18:26:01 +0200

    syntax error ex01

--- D05/ex01/ex01.sql
+++ D05/ex01/ex01.sql
@@ -1,3 +1,2 @@
 ## source /path/of/base_student.sql
-
-CREATE TABLE ft_table (id INT PRIMARY KEY AUTO_INCREMENT, login VARCHAR(8), group ENUM('staff', 'student', 'other'), date_de_creation DATE);
+CREATE TABLE ft_table ('id' INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 'login' VARCHAR(8) NOT NULL DEFAULT 'toto', 'group' ENUM('staff', 'student', 'other') NOT NULL, 'date_de_creation' DATE NOT NULL);
