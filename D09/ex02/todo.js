1209e12240cc10b443a76daf14bc023756cc9e10
tfolly <tfolly@student.42.fr>
2016-06-24 19:53:44 +0200

    todo list ok

--- D09/ex02/todo.js
+++ D09/ex02/todo.js
@@ -3,16 +3,16 @@ readcookie();
 function newTodo(){
 	var ft_list = document.getElementById('ft_list');
 	var new_task = prompt("Enter new task : ");
-	var new_elem = document.createElement('div');
-	new_elem.innerHTML = new_task;
-	new_elem.setAttribute("onclick", "delItem(this)");
-	ft_list.insertBefore(new_elem, ft_list.childNodes[0]);
-	savecookie(ft_list);
-	// console.log(ft_list);
+	if (/\S/.test(new_task)){
+		var new_elem = document.createElement('div');
+		new_elem.innerHTML = new_task;
+		new_elem.setAttribute("onclick", "delItem(this)");
+		ft_list.insertBefore(new_elem, ft_list.childNodes[0]);
+		savecookie(ft_list);
+	}
 }
 
 function delItem(itemtodel){
-	// console.log("delitem");
 	var ft_list = document.getElementById('ft_list');
 	var conf = confirm("Delete this task?");
 	if (conf){
@@ -24,38 +24,28 @@ function delItem(itemtodel){
 function savecookie(ft_list){
 	var cookie = "todo";
 	var elem;
-	test1 = ft_list;
-	for (var i = 0; i < ft_list.childNodes['length'] - 1; i++){
+	for (var i = 0; i < ft_list.childNodes['length']; i++){
 		elem = ft_list.childNodes[i].innerHTML;
-		// console.log(elem);
 		cookie = cookie.concat("/",elem);
 	}
-	test = cookie;
 	document.cookie = cookie;
 }
 
 function readcookie(){
 	var cookie = document.cookie;
 	var elem;
-	elem = cookie.match('todo[^;]*');
-		// console.log(elem);
+	elem = cookie.match('todo/([^;]*)');
+	test1 = elem;
 	if (elem){
-		elem = elem[0];
-		console.log(elem);
+		elem = elem[1];
 		elem = elem.split('/');
-		elem.shift();
-		console.log(elem);
 		var ft_list = document.getElementById('ft_list');
-		for (var task in elem)
+		for (task in elem)
 		{
-			// console.log(elem[task]);
 			var new_elem = document.createElement('div');
 			new_elem.innerHTML = elem[task];
 			new_elem.setAttribute("onclick", "delItem(this)");
-			// ft_list.insertBefore(new_elem, ft_list.childNodes[0]);
 			ft_list.appendChild(new_elem);
-			savecookie(ft_list);
 		}
 	}
-	// console.log(elem);
 }
