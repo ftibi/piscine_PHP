6d1e16226a684247be67e6e9176ff3814bc54f0e
tfolly <tfolly@student.42.fr>
2016-06-24 19:12:12 +0200

    pblm dans readcookie, ajoute undefined au debut a lappel

--- D09/ex02/todo.js
+++ D09/ex02/todo.js
@@ -8,26 +8,54 @@ function newTodo(){
 	new_elem.setAttribute("onclick", "delItem(this)");
 	ft_list.insertBefore(new_elem, ft_list.childNodes[0]);
 	savecookie(ft_list);
-	console.log(ft_list);
+	// console.log(ft_list);
 }
 
 function delItem(itemtodel){
 	// console.log("delitem");
+	var ft_list = document.getElementById('ft_list');
 	var conf = confirm("Delete this task?");
 	if (conf){
 		itemtodel.parentNode.removeChild(itemtodel);
-		savecookie();
+		savecookie(ft_list);
 	}
 }
 
 function savecookie(ft_list){
-	var cookie = document.cookie;
-	cookie('list', ft_list);
+	var cookie = "todo";
+	var elem;
+	test1 = ft_list;
+	for (var i = 0; i < ft_list.childNodes['length'] - 1; i++){
+		elem = ft_list.childNodes[i].innerHTML;
+		// console.log(elem);
+		cookie = cookie.concat("/",elem);
+	}
+	test = cookie;
+	document.cookie = cookie;
 }
 
 function readcookie(){
 	var cookie = document.cookie;
-	if (cookie.list){
-		console.log(cookie.list);
+	var elem;
+	elem = cookie.match('todo[^;]*');
+		// console.log(elem);
+	if (elem){
+		elem = elem[0];
+		console.log(elem);
+		elem = elem.split('/');
+		elem.shift();
+		console.log(elem);
+		var ft_list = document.getElementById('ft_list');
+		for (var task in elem)
+		{
+			// console.log(elem[task]);
+			var new_elem = document.createElement('div');
+			new_elem.innerHTML = elem[task];
+			new_elem.setAttribute("onclick", "delItem(this)");
+			// ft_list.insertBefore(new_elem, ft_list.childNodes[0]);
+			ft_list.appendChild(new_elem);
+			savecookie(ft_list);
+		}
 	}
+	// console.log(elem);
 }
