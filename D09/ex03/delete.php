a79ecda3438465b53d1cb906bf75d4d06a344d6c
tfolly <tfolly@student.42.fr>
2016-06-25 22:01:32 +0200

    push soir

--- D09/ex03/delete.php
+++ D09/ex03/delete.php
@@ -2,7 +2,8 @@
 
 if ($item = $_POST['itemtodel']){
 	$data = file_get_contents('list.csv');
-	$data = preg_replace('/\n.*;$item\n/', '\n', $data);
+	$data = preg_replace('/'.$item.';'.'.*\n/', '', $data);
+	// $data = preg_replace('\d*\.\d*;'.$item.'\n', '', $data);
 	file_put_contents('list.csv', $data);
 }
 
--- D09/ex03/todo.js
+++ D09/ex03/todo.js
@@ -3,11 +3,13 @@ readtask();
 function newtask(){
 	var ft_list = $('#ft_list');
 	var new_task = prompt("Enter new task : ");
-	savetask(new_task, event.timeStamp);
+	var time = event.timeStamp;
+	savetask(new_task, time);
 	if (/\S/.test(new_task)){
 		var new_elem = $('<div></div>');
 		new_elem.text(new_task);
 		new_elem.attr("onclick", "deltask(this)");
+		new_elem.attr("time", time);
 		new_elem.addClass('task');
 		ft_list.prepend(new_elem);
 	}
@@ -16,9 +18,10 @@ function newtask(){
 function deltask(itemtodel){
 	var conf = confirm("Delete this task?");
 	if (conf){
-		console.log(itemtodel.textContent);
+		var item = itemtodel.textContent;
+		item = itemtodel.attributes[1].textContent;
 		itemtodel.remove();
-		$.post('delete.php', {'itemtodel':itemtodel.textContent});
+		$.post('delete.php', {'itemtodel':item});
 	}
 }
 
@@ -32,19 +35,20 @@ function readtask(){
 		url: 'select.php',
 		data: 'select',
 		success: function (data){
-			data = data.replace(/\d*\.\d*;/g,'');
-			console.log(data);
-			data = data.replace(/\n/g,';');
-			console.log(data);
-			data = data.split(';');
-			console.log(data);
-			data = data.slice(0, -1);
+			var time;
+			data = data.split('\n');
+			data = data.filter(function(n){return n != ""});
 			var ft_list = $('#ft_list');
 			for (task in data)
 			{
+				time = data[task].match(/\d*\.\d*/);
+				time = time[0];
+				str = data[task].replace(time, '');
+				str = str.replace(';', '');
 				var new_elem = $('<div></div>');
-				new_elem.html(data[task]);
+				new_elem.html(str);
 				new_elem.attr("onclick", "deltask(this)");
+				new_elem.attr("time", time);
 				new_elem.addClass('task');
 				ft_list.append(new_elem);
 			}
